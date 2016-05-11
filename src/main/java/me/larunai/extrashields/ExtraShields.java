eb9496daa9100f882c142f8d487f92ad68269365
Larunai <larunai@web.de>
2016-05-11 13:36:20 +0200

    start_basic

---
+++ src/main/java/me/larunai/extrashields/ExtraShields.java
@@ -0,0 +1,43 @@
+package me.larunai.extrashields;
+
+import me.larunai.extrashields.proxy.CommonProxy;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = ExtraShieldsGlobal.MOD_ID, name = ExtraShieldsGlobal.MOD_NAME, version = ExtraShieldsGlobal.VERSION)
+public class ExtraShields {
+	
+	@Instance(ExtraShieldsGlobal.MOD_ID)
+	public static ExtraShields instance;
+	
+	@SidedProxy(clientSide = ExtraShieldsGlobal.EXTRASHIELDS_CLIENT_PROXY, serverSide = ExtraShieldsGlobal.EXTRASHIELDS_COMMON_PROXY)
+	public static CommonProxy proxy;
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent preEvent) {
+		
+		this.proxy.preInit(preEvent);
+		
+	}
+	
+	@EventHandler
+	public void init (FMLInitializationEvent event) {
+		
+		this.proxy.init(event);
+		
+	}
+	
+	@EventHandler
+	public void postInit (FMLPostInitializationEvent postEvent) {
+		
+		this.proxy.postInit(postEvent);
+		
+	}
+
+}
---
+++ src/main/java/me/larunai/extrashields/ExtraShieldsGlobal.java
@@ -0,0 +1,11 @@
+package me.larunai.extrashields;
+
+public class ExtraShieldsGlobal {
+	
+	public static final String MOD_ID = "extrashields";
+	public static final String MOD_NAME = "Extra Shields";
+	public static final String VERSION = "1.9 - 1.0";
+
+	public static final String EXTRASHIELDS_CLIENT_PROXY = "me.larunai.extrashields.proxy.ClientProxy";
+	public static final String EXTRASHIELDS_COMMON_PROXY = "me.larunai.extrashields.proxy.CommonProxy";
+}
